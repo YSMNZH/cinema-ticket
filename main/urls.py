98d0d8fc3e58275c9917205193f7aa17fadb6695
YSMNZH <ysmnzh@yahoo.com>
2024-11-27 11:56:51 +0330

    Contact Page Created.

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('main/', views.main, name='main'),
+    path('contact/', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.contrib.auth.decorators import login_required
 
 def main(request):
     return render(request, "main/main.html")
+
+def contact(request):
+    return render(request, "main/contact.html")
