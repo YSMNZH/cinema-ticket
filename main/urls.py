35ab8f239a09e0b52e4e8b6d96120450b66f9b35
YSMNZH <ysmnzh@yahoo.com>
2024-11-28 11:05:41 +0330

    News Page Created.

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('main/', views.main, name='main'),
     path('contact/', views.contact, name='contact'),
+    path('news/', views.news, name='news'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
 
 def main(request):
     return render(request, "main/main.html")
 
 def contact(request):
     return render(request, "main/contact.html")
+
+def news(request):
+    return render(request, "main/news.html")
