430e41ecff88319d20515cac6fc992df637703cb
YSMNZH <ysmnzh@yahoo.com>
2025-01-09 11:12:53 +0330

    Admin Panel Added.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Movie, Hall, Ticket
 
-# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Hall)
+admin.site.register(Ticket)
